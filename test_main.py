fb5232645bf99f1d5f327f2cea4ed04ecd82d901
himanshugohil10 <150456709+himanshugohil10@users.noreply.github.com>
2025-11-11 12:02:38 +0530

    Update expected response message in test_root

--- test_main.py
+++ test_main.py
@@ -6,7 +6,7 @@ client = TestClient(app)
 def test_root():
     response = client.get("/")
     assert response.status_code == 200
-    assert response.json() == {"message": "Hello this is your man!!"}
+    assert response.json() == {"message": "Please run!"}
 
 def test_health():
     response = client.get("/health")
